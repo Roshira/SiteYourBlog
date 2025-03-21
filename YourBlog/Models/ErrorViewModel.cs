f66814d09a3db558d9376eae71449466eb61328b
roshira <155329753+Roshira@users.noreply.github.com>
2025-03-21 19:56:42 +0200

    first commit

---
+++ YourBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YourBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
