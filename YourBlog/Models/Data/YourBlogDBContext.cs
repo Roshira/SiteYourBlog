9be59e5bff72b2b15db389be46563e0261424ccc
roshira <155329753+Roshira@users.noreply.github.com>
2025-03-22 20:50:23 +0200

    add data end DBContext without base

---
+++ YourBlog/Models/Data/YourBlogDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace YourBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+   
+    }
+}
